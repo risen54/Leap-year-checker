1adb2e73df3ab476b2abd06e86fc3e249b62c7e6
Satvik Pandey <90170287+Risen54@users.noreply.github.com>
2021-10-05 14:58:16 +0530

    Debugged

--- testing.py
+++ testing.py
@@ -7,10 +7,10 @@ The variable below checks that is the year divisible by four
 because a leap year occurs every 4 years which
 means that a leap year is divisible by four
 """
-check = year%4
 
 # The below if and elif statements check that the year when divided by 4 gives a remainder or not
 try:
+      check = int(year)%4
       if check == 0:
             print("The year is a leap year")
       elif check != 0:
